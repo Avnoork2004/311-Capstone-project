9a86a3f24c11141a73b02e5ed445f37193d6ffd6
Avnoor Kaur <Avnoork2004@gmail.com>
2024-10-07 15:10:08 -0400

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example._311_capstone_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example._311_capstone_project to javafx.fxml;
+    exports org.example._311_capstone_project;
+}
