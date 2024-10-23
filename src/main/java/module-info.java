65c21ee9644fe72b453fc09c60b9c598e9684a18
michelle Parisi <parimm@farmingdale.edu>
2024-10-23 10:06:50 -0400

    protoype

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example._311_capstone_project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens org.example._311_capstone_project to javafx.fxml;
--- src/main/java/org/example/_311_capstone_project/HelloController.java
+++ src/main/java/org/example/_311_capstone_project/HelloController.java
@@ -1,14 +1,62 @@
 package org.example._311_capstone_project;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+public class HelloController implements Initializable {
+                ArrayList<String> Movie = new ArrayList<>(
+                        Arrays.asList("John Wick, R","Jurassic Park, PG-13","Shrek, E","Maxxxine,R","Deadpool 3, R")
+
+
+                );
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView<String> MovieList;
+
+    @FXML
+    private TextField SearchBar;
+
+    @FXML
+    private Button Find;
+
+
+    @FXML
+    public void findMovie(javafx.event.ActionEvent event) {
+        MovieList.getItems().clear();
+
+
+        if (SearchBar.getText().isEmpty()) {
+            MovieList.getItems().addAll(Movie); // Show all movies if no search term
+        } else {
+            MovieList.getItems().addAll(search(SearchBar.getText(),Movie));
+        }
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        MovieList.getItems().addAll(Movie);
+
+    }
+
+    private List<String> search(String searchText,List<String> movieList) {
+
+        List<String> filteredsearchText = Arrays.asList(searchText.trim().toLowerCase().split(" "));
+        return movieList.stream().filter(input ->{
+            return filteredsearchText.stream().allMatch(Movie -> input.toLowerCase().contains(Movie.toLowerCase()));
+        }).collect(Collectors.toList());
+    }
+
+
 }
+
