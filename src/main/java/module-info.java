2ccd8982abe6b58c06343693b579846a9b19fa27
michelle Parisi <parimm@farmingdale.edu>
2024-11-24 12:54:18 -0500

    module error fix (master branch)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,7 @@ module org.example._311_capstone_project {
 
 
     opens org.example._311_capstone_project to javafx.fxml;
-    exports org.example._311_capstone_project;
+    //exports org.example._311_capstone_project;
     exports org.example._311_capstone_project.controller;
     opens org.example._311_capstone_project.controller to javafx.fxml;
 }
