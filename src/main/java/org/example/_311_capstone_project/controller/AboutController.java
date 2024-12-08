52db47ad3c94a5e1a9f26f5fb1e1df5e626a09c1
Avnoor Kaur <Avnoork2004@gmail.com>
2024-12-08 18:06:12 -0500

    updated close btn on about page so it goes back to the mainscreen after clicking

--- src/main/java/org/example/_311_capstone_project/controller/AboutController.java
+++ src/main/java/org/example/_311_capstone_project/controller/AboutController.java
@@ -2,8 +2,12 @@ package org.example._311_capstone_project.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
 
 public class AboutController {
 
@@ -15,7 +19,24 @@ public class AboutController {
 
     @FXML
     void closeAbout(ActionEvent event) {
+        try {
+            // Load the main screen scene
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/_311_capstone_project/mainscreen.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 895, 650);
 
+            // Get the current stage using the event source
+            Stage currentStage = (Stage) aboutBtn.getParentPopup().getOwnerWindow();
+            currentStage.close();
+
+            // Open the new stage for the main screen
+            Stage mainStage = new Stage();
+            mainStage.setScene(scene);
+            mainStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
